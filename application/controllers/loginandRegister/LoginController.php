d79d743ef8235eccb0579dff5af7f9d0f75d6ff1
Project-Dark-Owl <arieskurtmolina@gmail.com>
2021-09-17 00:26:48 +0800

    logn in part 1
    wla pa login pinapasok plng kung pano paganahin

---
+++ application/controllers/loginandRegister/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LoginController extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('template/header.php');
+        $this->load->view('template/header.php');
+        $this->load->view('template/footer.php');
+    }
+}
+
+
+?>
